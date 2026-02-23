78a9f4f3e4fa4f0ff35365247ca8d351dcdaf5af
werener <lenyazhuk@yandex.ru>
2026-02-23 13:27:20 +0300

    fix: better make; MVP argument parsing

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
-CXX = g++
-FLAGS = -std=c++11 -Wall
+cxx = g++
+Flags = -std=c++11 -Wall
 target = cdx
 
-$(target): build/$(target)
+all: main
+
+main: cdx
 
 clean:
 	rm -rf build/ 
     
 run: $(target)
-	./build/$(target)
+	@./build/$(target) $(args)
+
+.PHONY: all run clean
+
+$(target): build/$(target)
 
 build/$(target): src/main.cpp
 	@mkdir -p $(@D)
-	$(CXX) $(FLAGS) $< -o $@
+	$(cxx) $(Flags) $< -o $@
 	
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <filesystem>
-int main() {
-    std::cout << "Hello World!";
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+
+
+int main(int argc, char **argv) {
+    CLI::App app{"cdx"};
+    bool force{false};
+    app.add_flag("-f,--force", force, "No confirmation")
+        ->ignore_case();
+
+    CLI::App* list = app.add_subcommand("list", "List all your aliases");
+    
+    std::string path;
+    list->add_option("path", path, "Path to the dir");    
+
+    CLI11_PARSE(app, argc, argv);
+    std::cout << "Success!\n" 
+        << "path: " << path << std::endl; 
+
+    return 0;
 }
