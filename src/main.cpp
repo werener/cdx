1be4ab2746e2c415192e704c371c633ab3d16ff5
werener <lenyazhuk@yandex.ru>
2026-02-23 15:25:44 +0300

    refactor: remove obscolete code

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,13 @@
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
 #include "cli/list.hpp"
-
-int test(int argc, char **argv) {
-    CLI::App app{"cdx"};
-    bool force{false};
-    app.add_flag("-f,--force", force, "No confirmation")
-        ->ignore_case();
-
-    CLI::App* list = app.add_subcommand("list", "List all your aliases");
-    
-    std::string path;
-    list->add_option("path", path, "Path to the dir");    
-
-    CLI11_PARSE(app, argc, argv);
-    std::cout << "Success!\n" 
-        << "path: " << path << std::endl; 
-
-    return 0;
-}
+#include "cli/cdx.hpp"
 
 int main(int argc, char **argv) {
-    // test(argc, argv);
-
     CLI::App app{"cdx"};
 
     setup_list(app);
-
-    app.require_subcommand();
+    setup_cdx(app);
+    // app.require_subcommand();
     CLI11_PARSE(app, argc, argv);
 }
