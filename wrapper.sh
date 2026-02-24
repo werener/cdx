daac4132adfc80eb9353474042546c39897d7e25
werener <lenyazhuk@yandex.ru>
2026-02-24 20:49:31 +0300

    chore: update make and wrapper configuration

--- wrapper.sh
+++ wrapper.sh
@@ -1,10 +1,7 @@
 cdx() {
-    # Capture everything
-    raw_output=$(/mnt/c/Users/werener/Desktop/gitfolder/cdx/release/cdx "$@" 2>&1)
-    # Extract path (if exists) but don't show it
+    raw_output=$(/home/werener/Desktop/gitfolder/cdx/release/cdx "$@" 2>&1)
     path=$(echo "$raw_output" | sed -n 's/.*\[CDX_PATH\]\(.*\)\[\/CDX_PATH\].*/\1/p')
 
-    # Show everything EXCEPT the path and its delimiters
     clean_output=$(echo "$raw_output" | sed 's/\[CDX_PATH\].*\[\/CDX_PATH\]//g')
     echo -e "$clean_output"
     echo -e $path
